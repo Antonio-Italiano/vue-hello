35e0bceaefcdd3685558693aab62ea1cbcffe3fe
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-09 15:31:37 +0100

    Add bonus img data

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ console.log('Vue ok', Vue);
 const app = Vue.createApp({
     data() {
         return {
-            text:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            text:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            image:'https://vuejs.org/images/logo.png'
         }
     }
 });
